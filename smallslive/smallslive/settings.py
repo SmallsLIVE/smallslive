d2de5534f7796672e7a2ab2ddf8598b51ebce113
Filip Jukic <filip@jukic.me>
2014-03-18 18:12:27 +0100

    added users app

--- smallslive/smallslive/settings.py
+++ smallslive/smallslive/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = (
     'artists',
     'events',
     'old_site',
+    'users',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -87,7 +88,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
