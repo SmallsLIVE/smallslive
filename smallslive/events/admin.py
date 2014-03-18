5a33f0d874d871b0d06f905fa383b2d7a68998a3
Nate Aune <nate@appsembler.com>
2014-03-18 10:30:37 -0700

    make the events and artists show up in django admin

--- smallslive/events/admin.py
+++ smallslive/events/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from models import Event
+
 # Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    date_hierarchy = 'startday'
+    list_display = ('startday', 'title', 'subtitle', 'link', 'datefreeform', 'description')
+    list_filter = ('title',)
+    search_fields = ('title', 'subtitle')
+    # radio_fields = {'active': admin.HORIZONTAL}
+    # filter_horizontal = ('tags', 'speakers',)
+    save_on_top = True
+    # prepopulated_fields = {'slug': ('title',)}
+    # inlines = [RelatedUrlInline]
+    # actions = [make_live, make_draft]
+
+admin.site.register(Event, EventAdmin)
