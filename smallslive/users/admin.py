35032485663bf9af150149db14ca2900d0cea870
Nate Aune <nate@appsembler.com>
2014-03-19 15:54:25 -0700

    make it easier to override with local settings file

--- smallslive/users/admin.py
+++ smallslive/users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from models import UserProfile
+
 # Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    date_hierarchy = 'last_login'
+    list_display = ('user', 'access_level', 'login_count', 'subscription_price', 'start_date', 'renewal_date', 'active')
+    list_filter = ('access_level', 'active')
+    search_fields = ('user',)
+    # radio_fields = {'active': admin.HORIZONTAL}
+    # filter_horizontal = ('tags', 'speakers',)
+    save_on_top = True
+    # prepopulated_fields = {'slug': ('title',)}
+    # inlines = [RelatedUrlInline]
+    # actions = [make_live, make_draft]
+
+admin.site.register(UserProfile, UserProfileAdmin)
