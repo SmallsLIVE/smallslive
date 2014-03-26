9fcf48cee75ebafba320a3cb289a63e9b1aea8ce
Filip Jukic <filip@jukic.me>
2014-03-26 21:05:12 +0100

    some more heroku config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn smallslive.wsgi
--- smallslive/smallslive/settings/heroku.py
+++ smallslive/smallslive/settings/heroku.py
@@ -1,6 +1,25 @@
-from .base import *
 import os
+from .base import *
+import dj_database_url
+
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "herokudefault")
 
+# Parse database configuration from $DATABASE_URL
 DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
