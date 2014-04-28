dd2b4002a6afa4f3736bbc2fe22f993232b8c2db
BTO <spam@rhythmspice.com>
2014-04-28 15:48:37 -0400

    Minor update to search and changed admin view for Artists

--- smallslive/artists/admin.py
+++ smallslive/artists/admin.py
@@ -3,8 +3,8 @@ from models import Artist, Instrument
 
 
 class ArtistAdmin(admin.ModelAdmin):
-    list_display = ('salutation', 'first_name', 'last_name', 'biography')
-    list_display_links = ('salutation', 'first_name', 'last_name')
+    list_display = ('first_name', 'last_name', 'website','salutation','photo')
+    list_display_links = ('first_name', 'last_name')
     list_filter = ('instruments', 'last_name')
     search_fields = ('first_name', 'last_name')
     save_on_top = True
