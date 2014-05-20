9c30fed392305f5f97b67aedbb074bb4329af3f1
Filip Jukic <filip@jukic.me>
2014-05-20 17:03:50 +0200

    allow pages with "account" in the url to be displayed

--- smallslive/smallslive/settings/base.py
+++ smallslive/smallslive/settings/base.py
@@ -82,7 +82,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'djstripe.middleware.SubscriptionPaymentMiddleware',
+    #'djstripe.middleware.SubscriptionPaymentMiddleware',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
