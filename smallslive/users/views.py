f9be0aba804dae13ef6e626f09790cc01c449cac
Filip Jukic <filip@jukic.me>
2014-05-30 18:46:43 +0200

    redirect the user from the login page to artist dashboard

--- smallslive/users/views.py
+++ smallslive/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from allauth.account.views import LoginView as AllauthLoginView
 
-# Create your views here.
+
+class LoginView(AllauthLoginView):
+    success_url = reverse_lazy('static_page', kwargs={'template_name': 'dashboard-musician'})
+
+login_view = LoginView.as_view()
