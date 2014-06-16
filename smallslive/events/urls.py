3c28011492ae067e868de81c82a1f5dd0e3bb2f0
BTO <spam@rhythmspice.com>
2014-06-16 12:27:33 -0400

    Started to design and add data to Video Manager view; created invite-artists form; updated nav links;

--- smallslive/events/urls.py
+++ smallslive/events/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('events.views',
     url(r'^calendar/$', 'calendar', name='calendar'),
     url(r'^my-gigs/$', 'my_gigs', name='my_gigs'),
+    url(r'^video-manager/$', 'video_manager', name='video_manager'),
     url(r'^dashboard/$', 'venue_dashboard', name='venue_dashboard'),
     url(r'^(?P<pk>\d+)/$', 'event_detail', name='event_detail'),
     url(r'^(?P<pk>\d+)/edit/$', 'event_edit', name='event_edit'),
