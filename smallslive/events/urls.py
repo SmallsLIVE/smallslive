10f758c68e62d6a74d1874ca81a54980853b4cc1
BTO <spam@rhythmspice.com>
2014-06-17 16:54:34 -0400

    Renamed class for video manager

--- smallslive/events/urls.py
+++ smallslive/events/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('events.views',
     url(r'^calendar/$', 'calendar', name='calendar'),
     url(r'^my-gigs/$', 'my_gigs', name='my_gigs'),
-    url(r'^video-manager/$', 'video_manager', name='video_manager'),
+    url(r'^recordings/$', 'recordings', name='recordings'),
     url(r'^musician-signup-choose-videos/$', 'artist_video_manager', name='artist_video_manager'),
     url(r'^dashboard/$', 'venue_dashboard', name='venue_dashboard'),
     url(r'^(?P<pk>\d+)/$', 'event_detail', name='event_detail'),
