c72917d0a3ad4cfc47a9782c998103f14f9a739b
Filip Jukic <filip@jukic.me>
2014-06-25 18:00:48 +0200

    show correct file urls for media files in the admin

--- smallslive/multimedia/admin.py
+++ smallslive/multimedia/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import MediaFile
 
-admin.site.register(MediaFile)
+
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'media_type', 'format', 'get_file_url')
+    list_filter = ('media_type', 'format')
+
+
+admin.site.register(MediaFile, MediaAdmin)
