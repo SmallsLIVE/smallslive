ef213af5efa156d2b16cc047f0a3e1f57743d294
Filip Jukic <filip@jukic.me>
2014-08-09 14:56:48 +0200

    set password form test

--- smallslive/artist_registration/urls.py
+++ smallslive/artist_registration/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('artist_registration.views',
-    #url(r'^login/$', 'artist_login', name='artist_registration_login'),
+    url(r'^password-set/$', 'password_set', name='artist_registration_password_set'),
+    url(r'^confirm-email/(?P<key>\w+)/$', 'confirm_email', name='artist_registration_confirm_email'),
 )
