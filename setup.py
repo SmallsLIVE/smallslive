8935f0c0ea64f0c1dc872ca5035bc38d9ee520bd
Filip Jukic <filip@jukic.me>
2014-08-28 15:00:56 +0200

    version bump 0.1.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='smallslive',
-    version='0.1.6',
+    version='0.1.7',
     description=('Sample application for a music club'),
     author='Filip Jukic',
     author_email='filip@appsembler.com',
