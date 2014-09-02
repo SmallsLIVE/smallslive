f51c77a7527a775285888d921aa13c6ff8a855a5
Filip Jukic <filip@jukic.me>
2014-09-02 18:21:11 +0200

    version bump to 0.1.12

--- smallslive/__init__.py
+++ smallslive/__init__.py
@@ -2,4 +2,4 @@ import os
 
 SMALLSLIVE_TEMPLATE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 SMALLSLIVE_STATIC_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
-VERSION = '0.1.11'
+VERSION = '0.1.12'
