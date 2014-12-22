5cf6026031319d64a47dd33ebde1fffe97bad922
Filip Jukic <filip@jukic.me>
2014-12-22 17:16:51 +0100

    don't run migrations for tests

--- smallslive/smallslive/settings/testing.py
+++ smallslive/smallslive/settings/testing.py
@@ -11,3 +11,32 @@ DATABASES = {
 # not needed for tests
 STRIPE_PUBLISHABLE_KEY = ''
 STRIPE_SECRET_KEY = ''
+
+MIGRATION_MODULES = {'address': 'address.notmigrations',
+                     'admin': 'admin.notmigrations',
+                     'analytics': 'analytics.notmigrations',
+                     'artists': 'artists.notmigrations',
+                     'auth': 'auth.notmigrations',
+                     'basket': 'basket.notmigrations',
+                     'catalogue': 'catalogue.notmigrations',
+                     'contenttypes': 'contenttypes.notmigrations',
+                     'customer': 'customer.notmigrations',
+                     'django_extensions': 'django_extensions.notmigrations',
+                     'events': 'events.notmigrations',
+                     'facebook': 'facebook.notmigrations',
+                     'flatpages': 'flatpages.notmigrations',
+                     'multimedia': 'multimedia.notmigrations',
+                     'offer': 'offer.notmigrations',
+                     'order': 'order.notmigrations',
+                     'partner': 'partner.notmigrations',
+                     'payment': 'payment.notmigrations',
+                     'promotions': 'promotions.notmigrations',
+                     'reviews': 'reviews.notmigrations',
+                     'sessions': 'sessions.notmigrations',
+                     'shipping': 'shipping.notmigrations',
+                     'sites': 'sites.notmigrations',
+                     'socialaccount': 'socialaccount.notmigrations',
+                     'twitter': 'twitter.notmigrations',
+                     'users': 'users.notmigrations',
+                     'voucher': 'voucher.notmigrations',
+                     'wishlists': 'wishlists.notmigrations'}
