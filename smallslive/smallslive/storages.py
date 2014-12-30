6af9300fceb2a988e9131a9887aab22c41664b3b
Filip Jukic <filip@jukic.me>
2014-12-30 13:54:01 +0100

    switch whitenoise gzipmixin with pipeline's

--- smallslive/smallslive/storages.py
+++ smallslive/smallslive/storages.py
@@ -1,8 +1,7 @@
 from django.contrib.staticfiles.storage import ManifestStaticFilesStorage, StaticFilesStorage
-from pipeline.storage import PipelineMixin
-from whitenoise.django import GzipStaticFilesMixin
+from pipeline.storage import PipelineMixin, GZIPMixin
 
 
-class PipelineGzipStorage(GzipStaticFilesMixin, PipelineMixin, StaticFilesStorage):
+class PipelineGzipStorage(GZIPMixin, PipelineMixin, StaticFilesStorage):
     # don't normalize paths in the CSS file, make pipeline and manifest storage work together
     patterns = []
