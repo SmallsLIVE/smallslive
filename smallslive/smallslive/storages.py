b7b640754ab425ca22d9ae3423b6327599a11bc9
Filip Jukic <filip@jukic.me>
2014-12-30 09:31:09 +0100

    use normal storage instead of manifest

--- smallslive/smallslive/storages.py
+++ smallslive/smallslive/storages.py
@@ -1,8 +1,8 @@
-from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
+from django.contrib.staticfiles.storage import ManifestStaticFilesStorage, StaticFilesStorage
 from pipeline.storage import PipelineMixin
 from whitenoise.django import GzipStaticFilesMixin
 
 
-class PipelineGzipStorage(GzipStaticFilesMixin, PipelineMixin, ManifestStaticFilesStorage):
+class PipelineGzipStorage(GzipStaticFilesMixin, PipelineMixin, StaticFilesStorage):
     # don't normalize paths in the CSS file, make pipeline and manifest storage work together
     patterns = []
