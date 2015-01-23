6e8e241fc05d6e18c52213cca647e4dc68c6dd44
Filip Jukic <filip@jukic.me>
2015-01-23 16:37:28 +0100

    add staging settings

---
+++ smallslive/smallslive/settings/staging.py
@@ -0,0 +1,32 @@
+import os
+from .base import *
+import dj_database_url
+
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "herokudefault")
+
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+PIPELINE_ENABLED = False
+PIPELINE_SASS_BINARY = '/opt/venv/smallslive/bin/sassc'
+PIPELINE_SASS_ARGUMENTS = ''
+
+# disable oscar django compressor
+COMPRESS_ENABLED = False
+
+
+INSTALLED_APPS += (
+    'djrill',
+)
+
+# Email settings
+EMAIL_BACKEND = "djrill.mail.backends.djrill.DjrillBackend"
+MANDRILL_API_KEY = get_env_variable('MANDRILL_API_KEY')
