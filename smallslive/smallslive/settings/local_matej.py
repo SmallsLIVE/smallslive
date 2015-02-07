04098165edfaebf13219ef3da67de61d7288a085
Matej Grozdanović <grozdanowski@gmail.com>
2015-02-07 13:57:17 +0100

    add Matej's settings

---
+++ smallslive/smallslive/settings/local_matej.py
@@ -0,0 +1,31 @@
+import os
+from .base import *
+import dj_database_url
+
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "herokudefault")
+
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] = dj_database_url.config()
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+PIPELINE_ENABLED = False
+PIPELINE_SASS_BINARY = '/opt/venv/smallslive/bin/sassc'
+PIPELINE_SASS_ARGUMENTS = ''
+
+# disable oscar django compressor
+COMPRESS_ENABLED = False
+
+# Pipeline doesn't touch this, SASS compiled manually by gulp
+PIPELINE_CSS = {
+    'css': {
+        'source_filenames': (
+            'css/main.css',
+        ),
+        'output_filename': 'css/main.css',
+    },
+}
+
