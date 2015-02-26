0776400698cfb7943898a8588afa772ef130f8ee
Filip Jukic <filip@jukic.me>
2015-02-26 09:43:59 +0100

    remove unneeded allauth stuff

--- smallslive/users/views.py
+++
@@ -1,8 +0,0 @@
-from django.core.urlresolvers import reverse_lazy
-from allauth.account.views import LoginView as AllauthLoginView
-
-
-class LoginView(AllauthLoginView):
-    success_url = reverse_lazy('static_page', kwargs={'template_name': 'dashboard-musician'})
-
-login_view = LoginView.as_view()
