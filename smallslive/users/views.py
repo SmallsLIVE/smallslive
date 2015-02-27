e366afdd391cdc9c9ca880bf5899c5eaefebfa16
Filip Jukic <filip@jukic.me>
2015-02-27 01:28:46 +0100

    change password working

--- smallslive/users/views.py
+++ smallslive/users/views.py
@@ -1,4 +1,5 @@
 from allauth.account import app_settings
+from allauth.account.forms import SetPasswordForm
 from allauth.account.views import SignupView as AllauthSignupView, ConfirmEmailView as CoreConfirmEmailView
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -15,7 +16,7 @@ def user_settings_view(request):
     # if this is a POST request we need to process the form data
     if 'edit_profile' in request.POST:
         # create a form instance and populate it with data from the request:
-        edit_profile_form = EditProfileForm(request.POST, user=request.user)
+        edit_profile_form = EditProfileForm(data=request.POST, user=request.user)
         # check whether it's valid:
         if edit_profile_form.is_valid():
             edit_profile_form.save()
@@ -29,9 +30,18 @@ def user_settings_view(request):
     else:
         email_change_form = ChangeEmailForm(user=request.user)
 
+    if 'change_password' in request.POST:
+        change_password_form = SetPasswordForm(data=request.POST, user=request.user)
+        if change_password_form.is_valid():
+            change_password_form.save()
+            return HttpResponseRedirect('/')
+    else:
+        change_password_form = SetPasswordForm(user=request.user)
+
     return render(request, 'account/user_settings.html', {
         'email_change_form': email_change_form,
         'change_profile_form': edit_profile_form,
+        'change_password_form': change_password_form,
     })
 
 
