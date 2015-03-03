d640898436ee078d65d2216e096eeb423de4993f
Matej Grozdanović <grozdanowski@gmail.com>
2015-03-03 17:14:41 +0100

    add test header search autocomplete

--- smallslive/static/js/base.js
+++ smallslive/static/js/base.js
@@ -108,3 +108,85 @@ $('#schedule__date-picker input').datepicker({
 });
 
 $('#schedule__date-picker input').datepicker("setDate", new Date());
+
+
+/* Header search autocomplete */
+
+$.widget( "custom.catcomplete", $.ui.autocomplete, {
+    _create: function() {
+      this._super();
+      this.widget().menu( "option", "items", "> :not(.ui-autocomplete-category)" );
+    },
+    _renderMenu: function( ul, items ) {
+      var that = this,
+        currentCategory = "";
+      $.each( items, function( index, item ) {
+        var li;
+        if ( item.category != currentCategory ) {
+          ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
+          currentCategory = item.category;
+        }
+        li = that._renderItemData( ul, item );
+        if ( item.category ) {
+          li.attr( "aria-label", item.category + " : " + item.label );
+        }
+      });
+    }
+  });
+
+$(function() {
+    var searchData = [
+      { label: "Accordion", category: "Instruments" },
+      { label: "Alto Sax", category: "Instruments" },
+      { label: "Banjo", category: "Instruments" },
+      { label: "Baritone Sax", category: "Instruments" },
+      { label: "Bass", category: "Instruments" },
+      { label: "Bassoon", category: "Instruments" },
+      { label: "Cello", category: "Instruments" },
+      { label: "Clarinet", category: "Instruments" },
+      { label: "Composer", category: "Instruments" },
+      { label: "Conductor", category: "Instruments" },
+      { label: "Dancer", category: "Instruments" },
+      { label: "DJ", category: "Instruments" },
+      { label: "Drums", category: "Instruments" },
+      { label: "Female Vocalist", category: "Instruments" },
+      { label: "Flute", category: "Instruments" },
+      { label: "French Horn", category: "Instruments" },
+      { label: "Guitar", category: "Instruments" },
+      { label: "Harp", category: "Instruments" },
+      { label: "Jazz Orchestra", category: "Instruments" },
+      { label: "Leader", category: "Instruments" },
+      { label: "Lute", category: "Instruments" },
+      { label: "Male Vocalist", category: "Instruments" },
+      { label: "MC", category: "Instruments" },
+      { label: "Oboe", category: "Instruments" },
+      { label: "Orchestra", category: "Instruments" },
+      { label: "Organ", category: "Instruments" },
+      { label: "Percussion", category: "Instruments" },
+      { label: "Piano", category: "Instruments" },
+      { label: "Poet", category: "Instruments" },
+      { label: "Soprano Sax", category: "Instruments" },
+      { label: "String Quartet", category: "Instruments" },
+      { label: "Tap Dancer", category: "Instruments" },
+      { label: "Tenor Sax", category: "Instruments" },
+      { label: "Trombone", category: "Instruments" },
+      { label: "Trumpet", category: "Instruments" },
+      { label: "Vibraphone", category: "Instruments" },
+      { label: "Viola", category: "Instruments" },
+      { label: "Violin", category: "Instruments" },
+      { label: "Vocalist", category: "Instruments" },
+      { label: "Whistle", category: "Instruments" },
+      { label: "Nasar Abadey", category: "Artists" },
+      { label: "Carlos Abadie", category: "Artists" },
+      { label: "Rez Abbasi", category: "Artists" },
+      { label: "Brian Adler", category: "Artists" },
+      { label: "Cyrille Aimee", category: "Artists" },
+      { label: "Melissa Aldana", category: "Artists" },
+      { label: "Aaron Alexander", category: "Artists" }
+    ];
+
+    $( "#header--search" ).catcomplete({
+      delay: 0,
+      source: searchData
+    });
+  });
