cccc127984ebd0ea7f16b2735414a616f01d5c5b
Filip Jukic <filip@jukic.me>
2015-03-04 00:32:37 +0100

    fix instrument ajax for event add form

--- smallslive/artists/urls.py
+++ smallslive/artists/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('artists.views',
     url(r'^(?P<pk>\d+)-(?P<slug>[-\w]+)/edit/$', 'artist_edit', name='artist_edit'),
-    url(r'^(?P<pk>\d+)-(?P<slug>[-\w]+)/instrument_ajax/$', 'artist_instrument_ajax', name='artist_intrument_ajax'),
     url(r'^(?P<pk>\d+)-(?P<slug>[-\w]+)/$', 'artist_detail', name='artist_detail'),
+    url(r'^(?P<pk>\d+)/instrument_ajax/$', 'artist_instrument_ajax', name='artist_intrument_ajax'),
     url(r'^add/$', 'artist_add', name='artist_add'),
 )
