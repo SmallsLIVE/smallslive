66ab593c8cd07bfabfea5f7f458415fda625db1d
Filip Jukic <filip@jukic.me>
2015-03-26 13:52:26 +0100

    add artist dashboard namespace

--- smallslive/artist_dashboard/urls.py
+++ smallslive/artist_dashboard/urls.py
@@ -6,4 +6,7 @@ urlpatterns = patterns('artist_dashboard.views',
     url(r'^edit-profile/$', 'edit_profile', name='dashboard_edit_profile'),
     url(r'^my-gigs/$', 'my_gigs', name='dashboard_my_gigs'),
     url(r'^$', 'dashboard', name='dashboard_home'),
+    url(r'^edit-profile/$', 'edit_profile', name='edit_profile'),
+    url(r'^my-gigs/$', 'my_gigs', name='my_gigs'),
+    url(r'^$', 'dashboard', name='home'),
 )
