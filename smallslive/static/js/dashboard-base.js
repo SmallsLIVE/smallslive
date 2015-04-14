30f050b1f9c297c314a0683f03b2d3e4af03a7f4
Matej Grozdanović <grozdanowski@gmail.com>
2015-04-14 16:52:32 +0200

    artist dashboard - update legal page ux/ui

--- smallslive/static/js/dashboard-base.js
+++ smallslive/static/js/dashboard-base.js
@@ -74,3 +74,15 @@ $(document).ready(function () {
         }
     })
 })
+
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
