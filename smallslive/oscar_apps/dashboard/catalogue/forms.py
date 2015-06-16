a7197c9ed30f7215ad61d99271e85fabcef4bd0f
Filip Jukic <filip@jukic.me>
2015-06-16 17:44:38 +0200

    beginning of tracks UI in the oscar admin

--- smallslive/oscar_apps/dashboard/catalogue/forms.py
+++ smallslive/oscar_apps/dashboard/catalogue/forms.py
@@ -1,7 +1,27 @@
-from oscar.apps.dashboard.catalogue import forms
+from django import forms
+from django.forms import inlineformset_factory
+from oscar.apps.dashboard.catalogue import forms as oscar_forms
+from oscar.apps.dashboard.catalogue.forms import BaseProductImageFormSet
+from oscar_apps.catalogue.models import Product
 
 
-class ProductForm(forms.ProductForm):
-    class Meta(forms.ProductForm.Meta):
+class ProductForm(oscar_forms.ProductForm):
+    class Meta(oscar_forms.ProductForm.Meta):
         fields = [
             'title', 'upc', 'short_description', 'description', 'is_discountable', 'structure']
+
+
+class TrackForm(forms.Form):
+    track_no = forms.IntegerField(required=True)
+    title = forms.CharField(max_length=100, required=True)
+    author = forms.CharField(max_length=100, required=True)
+
+
+BaseTrackFormSet = inlineformset_factory(
+    Product, Product, form=TrackForm, extra=2)
+
+
+class TrackFormSet(BaseProductImageFormSet):
+
+    def __init__(self, product_class, user, *args, **kwargs):
+        super(TrackFormSet, self).__init__(*args, **kwargs)
