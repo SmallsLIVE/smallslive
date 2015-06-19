a54cafafd39e3df6b7833acd57d3db889aea7548
Filip Jukic <filip@jukic.me>
2015-06-19 21:42:55 +0200

    use S3 only when debug is disabled

--- smallslive/multimedia/fields.py
+++ smallslive/multimedia/fields.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from .s3_storages import AudioS3Storage, VideoS3Storage
 
@@ -5,10 +6,11 @@ from .s3_storages import AudioS3Storage, VideoS3Storage
 class DynamicBucketFileField(models.FileField):
 
     def pre_save(self, model_instance, add):
-        if model_instance.media_type == "audio":
-            storage = AudioS3Storage()
-        else:
-            storage = VideoS3Storage()
-        model_instance.file.storage = storage
-        model_instance.sd_video_file.storage = storage
+        if settings.DEBUG is False:
+            if model_instance.media_type == "audio":
+                storage = AudioS3Storage()
+            else:
+                storage = VideoS3Storage()
+            model_instance.file.storage = storage
+            model_instance.sd_video_file.storage = storage
         return super(DynamicBucketFileField, self).pre_save(model_instance, add)
