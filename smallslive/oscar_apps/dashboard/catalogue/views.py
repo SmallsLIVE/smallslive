0c17e4b060712d0467752f4500b1910cdd41ae57
Filip Jukic <filip@jukic.me>
2015-06-26 12:45:01 +0200

    fix forms when adding product (with and without tracks)

--- smallslive/oscar_apps/dashboard/catalogue/views.py
+++ smallslive/oscar_apps/dashboard/catalogue/views.py
@@ -6,5 +6,11 @@ class ProductCreateUpdateView(oscar_views.ProductCreateUpdateView):
     def get_context_data(self, **kwargs):
         if self.product_class.slug == 'album':
             self.formsets['track_formset'] = TrackFormSet
-        context = super(ProductCreateUpdateView, self).get_context_data(**kwargs)
-        return context
+        return super(ProductCreateUpdateView, self).get_context_data(**kwargs)
+
+    def process_all_forms(self, form):
+        if self.product_class.slug == 'album':
+            self.formsets['track_formset'] = TrackFormSet
+        return super(ProductCreateUpdateView, self).process_all_forms(form)
+
+    form_valid = form_invalid = process_all_forms
