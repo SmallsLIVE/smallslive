02fff97faba42ce4a9063a4b529e9ff4ec34d1b3
Filip Jukic <filip@jukic.me>
2015-06-26 10:58:22 +0200

    fix adding of nonmusic products

--- smallslive/oscar_apps/dashboard/catalogue/views.py
+++ smallslive/oscar_apps/dashboard/catalogue/views.py
@@ -1,15 +1,10 @@
 from oscar.apps.dashboard.catalogue import views as oscar_views
-from multimedia.forms import TrackFileForm
 from .forms import TrackFormSet
 
 
 class ProductCreateUpdateView(oscar_views.ProductCreateUpdateView):
-    def __init__(self, *args, **kwargs):
-        super(ProductCreateUpdateView, self).__init__(*args, **kwargs)
-        self.formsets['track_formset'] = TrackFormSet
-
     def get_context_data(self, **kwargs):
-        if self.product_class.slug != 'album':
-            del self.formsets['track_formset']
+        if self.product_class.slug == 'album':
+            self.formsets['track_formset'] = TrackFormSet
         context = super(ProductCreateUpdateView, self).get_context_data(**kwargs)
         return context
