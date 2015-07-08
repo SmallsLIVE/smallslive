35a4d10950ad5ae16f8424072af150c2583c74ed
Filip Jukic <filip@jukic.me>
2015-07-08 15:37:42 +0200

    show featured product in a parent category as well

--- smallslive/oscar_apps/catalogue/views.py
+++ smallslive/oscar_apps/catalogue/views.py
@@ -5,5 +5,5 @@ from oscar_apps.catalogue.models import Product
 class ProductCategoryView(catalogue_views.ProductCategoryView):
     def get_context_data(self, **kwargs):
         context = super(ProductCategoryView, self).get_context_data(**kwargs)
-        context['featured_product'] = Product.objects.filter(featured=True, categories=self.get_category()).first()
+        context['featured_product'] = Product.objects.filter(featured=True, categories__in=self.get_categories()).first()
         return context
