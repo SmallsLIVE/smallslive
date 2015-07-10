7fbec04b97ff3170e0e154f29e8c6f32cd5fb74d
Filip Jukic <filip@jukic.me>
2015-07-10 15:25:08 +0200

    show only merch category on homepage

--- smallslive/oscar_apps/promotions/views.py
+++ smallslive/oscar_apps/promotions/views.py
@@ -6,7 +6,7 @@ class HomeView(promotions_views.HomeView):
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['newest_physical_products'] = Product.objects.filter(
-            product_class__requires_shipping=True).order_by('-id')[:4]
+            product_class__slug='merchandise').order_by('-id')[:4]
         context['newest_recordings'] = Product.objects.filter(
             product_class__slug="album").order_by('-id')[:3]
         context['featured_recordings'] = Product.objects.filter(
