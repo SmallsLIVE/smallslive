801d5336c1060935d7dc2b480915633cd66a086f
Filip Jukić <filip@jukic.me>
2015-08-03 01:02:38 +0200

    datepicker update graph for event metrics

--- smallslive/static/js/metrics_datepicker.js
+++ smallslive/static/js/metrics_datepicker.js
@@ -5,9 +5,32 @@ $datePicker.datepicker({
     format: 'MM // yyyy',
     minViewMode: "months",
     orientation: "top auto",
-    startDate: "08/01/2015",
+    startDate: new Date(2015, 7-1, 1),
     autoclose: true
 });
 $datePicker.on('changeMonth', function(d){
-    console.log("changed date");
+    var countsURL = "http://localhost:9000/event_counts/";
+    var data = {
+        month: d.date.getMonth() + 1,
+        year: d.date.getFullYear(),
+        event_id: eventID
+    };
+    $.get(countsURL, data, function(data, textStatus, jqXHR) {
+        console.log(data);
+        playsData.labels = data.dates;
+        playsData.datasets[0].data = data.audio_plays_list;
+        playsData.datasets[1].data = data.video_plays_list;
+        playsData.datasets[2].data = data.total_plays_list;
+        minsData.labels = data.dates;
+        minsData.datasets[0].data = data.audio_seconds_list;
+        minsData.datasets[1].data = data.video_seconds_list;
+        minsData.datasets[2].data = data.total_seconds_list;
+        var playsActive = $("#metric-graph__show__data-1").hasClass("active");
+        if (playsActive) {
+            drawGraph(playsData);
+        } else {
+            drawGraph(minsData);
+        }
+
+    });
 });
