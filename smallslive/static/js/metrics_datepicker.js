ce9ee5b88bfbea9488851338de800d143807912d
Filip Jukić <filip@jukic.me>
2015-08-03 13:58:26 +0200

    working graph datepicker on my metrics

--- smallslive/static/js/metrics_datepicker.js
+++ smallslive/static/js/metrics_datepicker.js
@@ -1,4 +1,22 @@
 /* Bootstrap datepicker for Metrics */
+var drawGraph = function (data) {
+    var ctx = document.getElementById("graph-canvas").getContext("2d");
+    window.myLine = new Chart(ctx).Line(data, {
+        responsive: true,
+        maintainAspectRatio: false,
+        bezierCurve: false,
+        bezierCurveTension: 0.25,
+        scaleShowVerticalLines: false,
+        scaleOverride: false,
+        showScale: true,
+        pointDotRadius: 6,
+        datasetStrokeWidth: 4,
+        scaleFontSize: 10,
+        scaleFontStyle: "bold",
+        scaleFontColor: "#999",
+        multiTooltipTemplate: "<%if (label){%><%=datasetLabel%>: <%}%><%= value %> plays"
+    });
+};
 
 var $datePicker = $('#metric-graph__date-picker input');
 $datePicker.datepicker({
@@ -8,29 +26,7 @@ $datePicker.datepicker({
     startDate: new Date(2015, 7-1, 1),
     autoclose: true
 });
-$datePicker.on('changeMonth', function(d){
-    var countsURL = "http://localhost:9000/event_counts/";
-    var data = {
-        month: d.date.getMonth() + 1,
-        year: d.date.getFullYear(),
-        event_id: eventID
-    };
-    $.get(countsURL, data, function(data, textStatus, jqXHR) {
-        console.log(data);
-        playsData.labels = data.dates;
-        playsData.datasets[0].data = data.audio_plays_list;
-        playsData.datasets[1].data = data.video_plays_list;
-        playsData.datasets[2].data = data.total_plays_list;
-        minsData.labels = data.dates;
-        minsData.datasets[0].data = data.audio_seconds_list;
-        minsData.datasets[1].data = data.video_seconds_list;
-        minsData.datasets[2].data = data.total_seconds_list;
-        var playsActive = $("#metric-graph__show__data-1").hasClass("active");
-        if (playsActive) {
-            drawGraph(playsData);
-        } else {
-            drawGraph(minsData);
-        }
 
-    });
-});
+var date = new Date();
+$datePicker.datepicker("setDate", date);
+
