fdc304dc20d14b27246d79bd787b6497bcf4d419
Filip Jukić <filip@jukic.me>
2015-08-06 19:04:03 +0200

    featured merchandise on store homepage working

--- smallslive/oscar_apps/promotions/views.py
+++ smallslive/oscar_apps/promotions/views.py
@@ -5,10 +5,10 @@ from oscar_apps.catalogue.models import Product
 class HomeView(promotions_views.HomeView):
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['newest_physical_products'] = Product.objects.filter(
-            product_class__slug='merchandise').order_by('-id')[:4]
         context['newest_recordings'] = Product.objects.filter(
             product_class__slug="album").order_by('-id')[:3]
         context['featured_recordings'] = Product.objects.filter(
-            product_class__slug="album", featured=True).order_by('-id')[:4]
+            product_class__slug="album", featured=True)[:4]
+        context['featured_physical_products'] = Product.objects.filter(
+            product_class__slug="merchandise", featured=True)[:4]
         return context
