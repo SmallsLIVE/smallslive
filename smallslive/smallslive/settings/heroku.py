8d9d796f43c841cdb0f59aa1b096041b8395aec2
Filip Jukić <filip@jukic.me>
2015-08-08 22:09:52 +0200

    use correct metrics DB URL

--- smallslive/smallslive/settings/heroku.py
+++ smallslive/smallslive/settings/heroku.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "herokudefault")
 
 # Parse database configuration from $DATABASE_URL
 DATABASES['default'] = dj_database_url.config()
-DATABASES['metrics'] = dj_database_url.config('HEROKU_POSTGRESQL_PINK_URL')
+DATABASES['metrics'] = dj_database_url.config('METRICS_DB_URL')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
