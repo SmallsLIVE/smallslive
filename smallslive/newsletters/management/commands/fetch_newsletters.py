6cdea336d1f1f43b8033d4ec9c4b57fdbb708080
Filip Jukic <filip@jukic.me>
2015-09-22 14:42:44 -0400

    fetch newsletters once a week

--- smallslive/newsletters/management/commands/fetch_newsletters.py
+++ smallslive/newsletters/management/commands/fetch_newsletters.py
@@ -1,5 +1,7 @@
+import os
 from django.conf import settings
 from django.core.management.base import NoArgsCommand
+from django.utils import timezone
 from mailchimp import Mailchimp
 from newsletters.models import Newsletter
 
@@ -8,6 +10,13 @@ class Command(NoArgsCommand):
     help = 'Fetch the newsletters from MailChimp'
 
     def handle_noargs(self, *args, **options):
+        env = os.environ.get('CRON_ENV')
+        now = timezone.now()
+        # heroku scheduler launches the task every day, we make sure it only really does the import
+        # once a week (tuesday)
+        if env == "heroku" and now.weekday() != 1:
+            return
+
         mc = Mailchimp(settings.MAILCHIMP_API_KEY)
         campaigns = mc.campaigns.list(filters={
             'status': 'sent',
