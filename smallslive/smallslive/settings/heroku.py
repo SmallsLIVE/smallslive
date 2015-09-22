c94ba03649b51190d1affe29ec52348327dca6a3
Filip Jukic <filip@jukic.me>
2015-09-22 16:14:14 -0400

    enable redis caching with cacheops

--- smallslive/smallslive/settings/heroku.py
+++ smallslive/smallslive/settings/heroku.py
@@ -1,4 +1,5 @@
 import os
+import urlparse
 from .base import *
 import dj_database_url
 
@@ -57,4 +58,38 @@ DEFAULT_FROM_EMAIL = 'smallslive@gmail.com'
 DEFAULT_FROM_REGISTRATION_EMAIL = "smallsliveusers@gmail.com"
 
 # Metrics
-METRICS_SERVER_URL = "http://metrics.smallslive.com" # no trailing slash
+METRICS_SERVER_URL = "http://metrics.smallslive.com"  # no trailing slash
+
+# Cache
+redis_url = urlparse.urlparse(get_env_variable('REDIS_URL'))
+CACHEOPS_REDIS = {
+    'host': redis_url.hostname ,
+    'port': redis_url.port,
+    'db': 1,
+    'password': redis_url.password,
+    'socket_timeout': 5,   # connection timeout in seconds, optional
+}
+
+CACHEOPS = {
+    # Automatically cache any User.objects.get() calls for 15 minutes
+    # This includes request.user or post.author access,
+    # where Post.author is a foreign key to auth.User
+    'auth.user': {'ops': 'get', 'timeout': 60*15},
+
+    # Automatically cache all gets and queryset fetches
+    # to other django.contrib.auth models for an hour
+    'auth.*': {'ops': ('fetch', 'get'), 'timeout': 60*60},
+
+    # Cache gets, fetches, counts and exists to Permission
+    # 'all' is just an alias for ('get', 'fetch', 'count', 'exists')
+    'auth.permission': {'ops': 'all', 'timeout': 60*60},
+
+    'artists.*': {'ops': 'all', 'timeout': 5*60},
+    'events.*': {'ops': 'all', 'timeout': 5*60},
+    'multimedia.*': {'ops': 'all', 'timeout': 5*60},
+
+    'catalogue.*': {'ops': 'all', 'timeout': 5*60},
+
+    # And since ops is empty by default you can rewrite last line as:
+    '*.*': {'timeout': 60*60},
+}
--- smallslive/smallslive/settings/local_filip.py
+++ smallslive/smallslive/settings/local_filip.py
@@ -32,6 +32,7 @@ DATABASES = {
 DATABASES['heroku'] = dj_database_url.config()
 
 INSTALLED_APPS += (
+    'cacheops',
     'debug_toolbar',
     'devserver',
 )
@@ -96,3 +97,35 @@ PIPELINE_CSS = {
 
 # Metrics
 METRICS_SERVER_URL = "http://localhost:9000"  # no trailing slash
+
+# Cache
+CACHEOPS_REDIS = {
+    'host': '192.168.59.103', # redis-server is on same machine
+    'port': 32768,        # default redis port
+    'db': 1,             # SELECT non-default redis database
+                         # using separate redis db or redis instance
+                         # is highly recommended
+
+    'socket_timeout': 10,   # connection timeout in seconds, optional
+}
+
+CACHEOPS = {
+    # Automatically cache any User.objects.get() calls for 15 minutes
+    # This includes request.user or post.author access,
+    # where Post.author is a foreign key to auth.User
+    'auth.user': {'ops': 'get', 'timeout': 60*15},
+
+    # Automatically cache all gets and queryset fetches
+    # to other django.contrib.auth models for an hour
+    'auth.*': {'ops': ('fetch', 'get'), 'timeout': 60*60},
+
+    # Cache gets, fetches, counts and exists to Permission
+    # 'all' is just an alias for ('get', 'fetch', 'count', 'exists')
+    'auth.permission': {'ops': 'all', 'timeout': 60*60},
+
+    'artists.*': {'ops': 'all', 'timeout': 15*60},
+    'events.*': {'ops': 'all', 'timeout': 120*60},
+
+    # And since ops is empty by default you can rewrite last line as:
+    '*.*': {'timeout': 60*60},
+}
