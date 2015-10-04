e0c7d4ea8e616c2b96d01bfabc8e266258c94768
Filip Jukić <filip@jukic.me>
2015-10-04 12:56:47 +0200

    fix video watching for paid subscribers

--- smallslive/users/models.py
+++ smallslive/users/models.py
@@ -178,7 +178,7 @@ class SmallsUser(AbstractBaseUser, PermissionsMixin):
     @cached_property
     def can_watch_video(self):
         return self.is_staff or self.is_artist or (
-            self.has_activated_account and self.get_subscription_plan['type'] == 'premium')
+            self.has_activated_account and self.get_subscription_plan['type'] != 'free')
 
     @cached_property
     def can_listen_to_audio(self):
