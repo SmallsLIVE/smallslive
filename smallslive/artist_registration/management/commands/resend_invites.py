9fab8a8a2a9aed02f4444d2c3e8524b6c49472ec
Filip Jukić <filip@jukic.me>
2015-10-05 19:06:40 +0200

    add command for resending invites

---
+++ smallslive/artist_registration/management/commands/resend_invites.py
@@ -0,0 +1,21 @@
+import time
+from django.core.management.base import NoArgsCommand
+from django.http import HttpRequest
+from users.models import SmallsUser, SmallsEmailAddress
+
+
+class Command(NoArgsCommand):
+    help = ''
+
+    def handle_noargs(self, *args, **options):
+        artists = SmallsUser.objects.exclude(artist=None).filter(emailaddress__verified=False)
+        print artists
+        req = HttpRequest()
+        req.session = {}
+        req.META['SERVER_NAME'] = 'beta.smallslive.com'
+        req.META['SERVER_PORT'] = '80'
+        for user in artists:
+            print "Sending {0}".format(user.email)
+            email = SmallsEmailAddress.objects.get(user=user, email=user.email)
+            email.send_confirmation(req, signup=True, activate_view="artist_registration_confirm_email")
+            time.sleep(1)
