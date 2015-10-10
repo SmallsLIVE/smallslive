0eb9ff187429396b177e218121cd45236114484c
Filip Jukić <filip@jukic.me>
2015-10-10 04:23:05 +0200

    fix old redirect views

--- smallslive/utils/views.py
+++ smallslive/utils/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # URL redirects from old site
 from django.views.generic import RedirectView
-from rest_framework.reverse import reverse_lazy
+from rest_framework.reverse import reverse
 from artists.models import Artist
 
 
@@ -12,11 +12,11 @@ class OldSiteRedirectView(RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
         if self.request.GET.get('itemCategory') == '43178' or  self.request.GET.get('itemcategory') == '43178':
-            return reverse_lazy('contact-and-info')
+            return reverse('contact-and-info')
         elif self.request.GET.get('itemCategory') == '43179' or self.request.GET.get('itemcategory') == '43179':
-            return reverse_lazy('live-stream')
+            return reverse('live-stream')
         elif self.request.GET.get('itemCategory') == '32321' or self.request.GET.get('itemcategory') == '32321':
-            return reverse_lazy('live-stream')
+            return reverse('live-stream')
         elif (self.request.GET.get('itemCategory') == '61473' or self.request.GET.get('itemcategory') == '61473') \
                 and self.request.GET.get('personDetailId'):
             try:
@@ -24,4 +24,4 @@ class OldSiteRedirectView(RedirectView):
                 return artist.get_absolute_url()
             except Artist.DoesNotExist:
                 raise Http404("Can't find the artist")
-        return reverse_lazy('home')
+        return reverse('home')
