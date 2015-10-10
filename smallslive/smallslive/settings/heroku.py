b355160829d427e3f19d1eb7200621504f87081e
Filip Jukić <filip@jukic.me>
2015-10-10 15:07:25 +0200

    remove prepend www

--- smallslive/smallslive/settings/heroku.py
+++ smallslive/smallslive/settings/heroku.py
@@ -26,7 +26,6 @@ DATABASES['metrics'] = dj_database_url.config('METRICS_DB_URL')
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 MIDDLEWARE_CLASSES = ('sslify.middleware.SSLifyMiddleware',) + MIDDLEWARE_CLASSES
-PREPEND_WWW = True
 
 
 # Allow all host headers
