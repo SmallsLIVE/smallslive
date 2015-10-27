3258f2accde1739e56c3df857d6d0606afa18294
Filip Jukić <filip@jukic.me>
2015-10-27 12:39:41 +0100

    add cacheops to Matej's settings

--- smallslive/smallslive/settings/local_matej.py
+++ smallslive/smallslive/settings/local_matej.py
@@ -14,6 +14,7 @@ DATABASES['default'] = dj_database_url.config()
 DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
 
 INSTALLED_APPS += (
+    'cacheops',
     'debug_toolbar',
 )
 
@@ -53,3 +54,38 @@ PIPELINE_CSS = {
         'output_filename': 'css/dashboard.css',
     },
 }
+
+
+# Cache
+CACHEOPS_REDIS = {
+    'host': os.environ.get("REDIS_IP"),  # redis-server is on same machine
+    'port': os.environ.get("REDIS_PORT"),        # default redis port
+    'db': 1,             # SELECT non-default redis database
+                         # using separate redis db or redis instance
+                         # is highly recommended
+
+    'socket_timeout': 10,   # connection timeout in seconds, optional
+}
+
+CACHEOPS = {
+    # Automatically cache any User.objects.get() calls for 15 minutes
+    # This includes request.user or post.author access,
+    # where Post.author is a foreign key to auth.User
+    'auth.user': {'ops': 'get', 'timeout': 60*15},
+
+    # Automatically cache all gets and queryset fetches
+    # to other django.contrib.auth models for an hour
+    'auth.*': {'ops': ('fetch', 'get'), 'timeout': 60*60},
+
+    # Cache gets, fetches, counts and exists to Permission
+    # 'all' is just an alias for ('get', 'fetch', 'count', 'exists')
+    'auth.permission': {'ops': 'all', 'timeout': 60*60},
+
+    'artists.*': {'ops': 'all', 'timeout': 15*60},
+    'events.*': {'ops': 'all', 'timeout': 120*60},
+
+    # And since ops is empty by default you can rewrite last line as:
+    '*.*': {'timeout': 60*60},
+}
+CACHEOPS_DEGRADE_ON_FAILURE = True
+CACHEOPS_FAKE = True
