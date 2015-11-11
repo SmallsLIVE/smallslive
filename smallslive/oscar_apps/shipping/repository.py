cb1267988f27c0952fe5db968ee9fdc085ad809e
Filip Jukić <filip@jukic.me>
2015-11-11 15:04:29 +0100

    free domestic and paid international shipping added

--- smallslive/oscar_apps/shipping/repository.py
+++ smallslive/oscar_apps/shipping/repository.py
@@ -3,9 +3,14 @@ from decimal import Decimal as D
 from oscar.apps.shipping import repository, methods, models
 
 
-class Standard(methods.FixedPrice):
-    code = "standard"
-    name = "Standard"
+class Domestic(methods.Free):
+    code = "domestic"
+    name = "Domestic"
+
+
+class International(methods.FixedPrice):
+    ode = "international"
+    name = "International"
     charge_excl_tax = D('10.00')
     charge_incl_tax = D('10.00')
 
@@ -14,4 +19,11 @@ class Repository(repository.Repository):
 
     def get_available_shipping_methods(
             self, basket, shipping_addr=None, **kwargs):
-        return [Standard()]
+        if shipping_addr:
+            if shipping_addr.country.code == 'US':
+                methods = (Domestic(),)
+            else:
+                methods = (International(),)
+        else:
+            methods = self.methods
+        return methods
