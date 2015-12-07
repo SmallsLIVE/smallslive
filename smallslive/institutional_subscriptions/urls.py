abfb954d3083d23383977a0b45b7c5de97820ca5
Filip Jukić <filip@jukic.me>
2015-12-07 19:26:13 +0100

    members list for institutions

--- smallslive/institutional_subscriptions/urls.py
+++ smallslive/institutional_subscriptions/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('institutional_subscriptions.views',
+    url(r"^institutions/(?P<pk>\d+)/members/$", 'institution_members',
+        name="institution_members"),
     url(r"^institutions/$", 'institution_list',
         name="institutions"),
-#    url(r"^media-redirect/(?P<recording_id>\d+)/$", 'media_redirect',
-#        name="media_redirect"),
 )
--- smallslive/institutional_subscriptions/views.py
+++ smallslive/institutional_subscriptions/views.py
@@ -1,5 +1,8 @@
 from braces.views import StaffuserRequiredMixin
+from django.shortcuts import get_object_or_404
 from django.views.generic.list import ListView
+
+from users.models import SmallsUser
 from .models import Institution
 
 
@@ -10,3 +13,15 @@ class InstitutionsListView(StaffuserRequiredMixin, ListView):
     paginate_by = 30
 
 institution_list = InstitutionsListView.as_view()
+
+
+class InstitutionMembersList(StaffuserRequiredMixin, ListView):
+    template_name = "institutional_subscriptions/members_list.html"
+    context_object_name = "members"
+    paginate_by = 50
+
+    def get_queryset(self):
+        institution = get_object_or_404(Institution, pk=self.kwargs.get('pk'))
+        return SmallsUser.objects.filter(institution=institution)
+
+institution_members = InstitutionMembersList.as_view()
