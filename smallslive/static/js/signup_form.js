7cefb06aab82c437db720ed64e0f1b46c0d7557e
Guillermo del Fresno <gdelfresno@sophilabs.com>
2018-05-09 14:45:33 -0300

    Refactor ajax sign up for forms.

--- smallslive/static/js/signup_form.js
+++ smallslive/static/js/signup_form.js
@@ -1,33 +1,36 @@
-$loginForm = $('#form-login');
-
-$loginForm.submit(function(e){
+function getAjaxSubmitForForm(form, formFields) {
+  return function ajaxSubmit(e) {
     e.preventDefault();
-    $("#login-error").addClass('hidden');
-    $("#password-error").addClass('hidden');
+    formFields.forEach(function (fieldName) {
+      $("#" + fieldName + "-error").addClass('hidden');
+    });
     $.ajax({
-        type: "POST",
-        url: $loginForm.attr('action'),
-        data: $loginForm.serialize(),
-        success: function(data) {
-            window.location = data.location;
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            var response = jqXHR.responseJSON;
-            //$("#login-modal").html(jqXHR.responseJSON.html);
-            if (response.form_errors.login) {
-                $("input[name=login]").parent().addClass('has-error');
-                $("#login-error").removeClass('hidden').text(response.form_errors.login[0]);
-            }
-            if (response.form_errors.password) {
-                $("input[name=password]").parent().addClass('has-error');
-                $("#password-error").removeClass('hidden').text(response.form_errors.password[0]);
-            }
-            if (response.form_errors.__all__) {
-                $("input[name=password]").parent().addClass('has-error');
-                $("#password-error").removeClass('hidden').text(response.form_errors.__all__[0]);
-            }
+      type: "POST",
+      url: form.attr('action'),
+      data: form.serialize(),
+      success: function (data) {
+        window.location = data.location;
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        var response = jqXHR.responseJSON;
+        formFields.forEach(function (fieldName) {
+          if (response.form_errors[fieldName]) {
+            $("input[name=" + fieldName + "]").parent().addClass('has-error');
+            $("#" + fieldName + "-error").removeClass('hidden').text(response.form_errors[fieldName][0]);
+          }
+        });
+
+        if (response.form_errors.__all__) {
+          $("input[name=password1]").parent().addClass('has-error');
+          $("#password1-error").removeClass('hidden').text(response.form_errors.__all__[0]);
         }
+      }
     });
     return false;
 
-});
+  }
+}
+
+$loginForm = $('#form-login');
+$loginForm.submit(getAjaxSubmitForForm($loginForm, ["login", "password"]));
+
