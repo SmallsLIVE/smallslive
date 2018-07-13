2d61afe86b08a163ee03803c0f7ba39636cc571e
Guillermo del Fresno <gdelfresno@sophilabs.com>
2018-07-13 10:53:34 -0300

    Refine metrics graph styles

--- smallslive/static/js/metrics_datepicker.js
+++ smallslive/static/js/metrics_datepicker.js
@@ -1,10 +1,29 @@
 /* Bootstrap datepicker for Metrics */
 var drawGraph = function (data, label) {
     var ctx = document.getElementById("graph-canvas").getContext("2d");
+    var graphPadding = 100;
+    Chart.defaults.global.defaultFontFamily = "'Reem Kufi', sans-serif";
+    Chart.defaults.global.defaultFontColor = 'black';
     var config = {
         type: 'line',
         data: data,
         options: {
+            legend: {
+                display: false
+            },
+            elements: {
+              line: {
+                  tension: 0
+              }
+            },
+            layout: {
+                padding: {
+                    left: 10,
+                    right: graphPadding,
+                    top: graphPadding,
+                    bottom: 10
+                }
+            },
             responsive: true,
             maintainAspectRatio: false,
             bezierCurve: false,
@@ -23,12 +42,40 @@ var drawGraph = function (data, label) {
                     type: 'time',
                     time: {
                         minUnit: 'day'
+                    },
+                    gridLines: {
+                        display: false
+                    },
+                    ticks: {
+                        padding: 20,
+                        fontSize: 12,
+                        callback: function(value, index, values) {
+                            return value.toUpperCase();
+                        }
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'DATE DURATION',
+                        padding: 50
                     }
                 }],
                 yAxes: [{
                     ticks: {
                         beginAtZero: true,
-                        suggestedMax: 10
+                        suggestedMax: 10,
+                        fontSize: 12,
+                        padding: 15
+                    },
+                    gridLines: {
+                        lineWidth: 4,
+                        drawTicks: false,
+                        color: 'white',
+                        drawBorder: false
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'SECONDS PLAYED',
+                        padding: 50
                     }
                 }]
             }
@@ -37,9 +84,26 @@ var drawGraph = function (data, label) {
     window.myLine = new Chart(ctx, config);
 };
 
+var checkPoints = function(chart, data, datasetIndex, remove) {
+  chart.data.datasets[datasetIndex].pointBackgroundColor = [];
+  chart.data.datasets[datasetIndex].pointRadius = [];
+  for (var i = 1; i <= data.datasets[datasetIndex].data.length - 1; i++) {
+    if (data.datasets[datasetIndex].data[i - 1] === data.datasets[datasetIndex].data[i]) {
+      if (remove) {
+        chart.data.datasets[datasetIndex].pointRadius[i] = 0;
+      }
+    } else {
+      chart.data.datasets[datasetIndex].pointBackgroundColor[i] = 'black';
+    }
+  }
+};
+
 var updateGraph = function (data) {
     window.myLine.data = data;
+    checkPoints(window.myLine, data, 2, true);
     window.myLine.update();
+
+
 };
 
 var $datePicker = $('#metric-graph__date-picker input');
