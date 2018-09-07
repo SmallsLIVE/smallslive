d5d021bf12b0d1ef28abb0ca7acd46febcd65b3d
Martin Prunell <martin.prunell@gmail.com>
2018-09-07 14:02:24 -0300

    Adust merge for demo

--- smallslive/search/urls.py
+++ smallslive/search/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('search.views',
     url(r'^autocomplete/$', 'search_autocomplete', name='search_autocomplete'),
     url(r'^$', views.TemplateSearchView.as_view(), name='search'),
     url(r'^ajax/search-bar/$', views.SearchBarView.as_view(), name='search-bar-ajax'),
+    url(r'^ajax/artist-info/$', views.ArtistInfo.as_view(), name='artist-info-ajax'),
     url(r'^ajax/(?P<entity>[\w\-]+)/$', views.MainSearchView.as_view(), name='search-ajax'),
 )
