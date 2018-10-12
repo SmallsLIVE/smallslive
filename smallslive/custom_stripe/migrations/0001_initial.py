e3159cbddf93882c9b8965317dc3a856d549ead5
Ignacio Arzuaga <iarzuaga@sophilabs.com>
2018-10-12 14:05:27 -0300

    Small fix

--- smallslive/custom_stripe/migrations/0001_initial.py
+++ smallslive/custom_stripe/migrations/0001_initial.py
@@ -7,10 +7,6 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-<<<<<<< HEAD
-        ('djstripe', '0004_auto_20180912_1357'),
-=======
->>>>>>> develop
     ]
 
     operations = [
@@ -26,19 +22,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CustomPlan',
             fields=[
-<<<<<<< HEAD
-                ('plan_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='djstripe.Plan')),
-            ],
-            options={
-                'abstract': False,
-            },
-            bases=('djstripe.plan',),
-=======
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
             ],
             options={
             },
             bases=(models.Model,),
->>>>>>> develop
         ),
     ]
