2ba87a4d1d15b88755b1197e2552187bf78459b0
Ignacio Arzuaga <iarzuaga@sophilabs.com>
2018-11-19 14:57:06 -0300

    Merged changes

--- smallslive/static/js/search/search.js
+++ smallslive/static/js/search/search.js
@@ -369,20 +369,12 @@ $(document).ready(function () {
     $(".instrument-btn").click(function () {
 
         if (!$(".instruments-container").is(":visible")) {
-<<<<<<< HEAD
-            if( viewPortLength("width") < 960){
-=======
-            if(window.screen.availWidth < 1024){
->>>>>>> 8b91ffd22f852924d45f0951d9872e5a574bbdae
+            if( viewPortLength("width") < 1024){
                 $("body").addClass("hidden-body");
             }
             $(".instruments-container").css("display", "flex");
         } else {
-<<<<<<< HEAD
-            if( viewPortLength("width") < 960){
-=======
-            if(window.screen.availWidth < 1024){
->>>>>>> 8b91ffd22f852924d45f0951d9872e5a574bbdae
+            if( viewPortLength("width") < 1024){
                 $("body").removeClass("hidden-body");
             }
             $(".instruments-container").css("display", "none");
@@ -401,21 +393,13 @@ $(document).ready(function () {
     });
 
     $('.instruments-container .close-button').click(function () {
-<<<<<<< HEAD
-        if( viewPortLength("width") < 960){
-=======
-        if(window.screen.availWidth < 1024){
->>>>>>> 8b91ffd22f852924d45f0951d9872e5a574bbdae
+        if( viewPortLength("width") < 1024){
         $("body").removeClass("hidden-body");
         }
     });
 
     $('.instrument').click(function () {
-<<<<<<< HEAD
-        if( viewPortLength("width") < 960){
-=======
-        if(window.screen.availWidth < 1024){
->>>>>>> 8b91ffd22f852924d45f0951d9872e5a574bbdae
+        if( viewPortLength("width") < 1024){
             $("body").removeClass("hidden-body");
         }
         artistInstrument = $(this).data('instrument');
@@ -564,11 +548,7 @@ $(document).ready(function () {
           apply = true;
           eventPageNum = 1;
           $('[data-toggle-tab-group="search-results"][data-toggle-tab-target]').show();
-<<<<<<< HEAD
-          if( viewPortLength("width") < 960){
-=======
-          if(window.screen.availWidth < 1024){
->>>>>>> 8b91ffd22f852924d45f0951d9872e5a574bbdae
+          if( viewPortLength("width") < 1024){
             $('[data-toggle-tab-group="search-results"][data-toggle-tab]').hide();
           }
           $('[data-toggle-tab-group="search-results"][data-toggle-tab="musicians"]').show();
