d2b7f6825360b5c0238f1104ec4feef8a6e1c694
mmartinez <mmartinez@sophilabs.com>
2018-12-05 14:40:29 -0300

    solved archive issue ( fails when click archive button )

--- smallslive/search/search.py
+++ smallslive/search/search.py
@@ -165,11 +165,12 @@ class SearchObject(object):
         # sets number_of_performers_searched based in the last word from main_seach
         number_of_performers_searched = None
         posible_number_of_performers = ['solo', 'duo', 'trio', 'quartet', 'quintet', 'sextet', 'septet', 'octet', 'nonet', 'dectet']
-        if main_search.split()[-1] in posible_number_of_performers:
-            number_of_performers_searched = posible_number_of_performers.index(main_search.split()[-1]) + 1
-            if ''.join(main_search.split()[:-1]) != '':
-                main_search = ' '.join(main_search.split()[:-1])
-        sqs = ''
+        if main_search != '':
+            if main_search.split()[-1] in posible_number_of_performers:
+                number_of_performers_searched = posible_number_of_performers.index(main_search.split()[-1]) + 1
+                if ''.join(main_search.split()[:-1]) != '':
+                    main_search = ' '.join(main_search.split()[:-1])
+            sqs = ''
         
         
 
@@ -239,11 +240,11 @@ class SearchObject(object):
         if number_of_performers_searched:
                 sqs = search_by_number_musicians(number_of_performers_searched, main_search)
                 sqs = sqs.order_by(order)
-        # FIXME: compare to code in  "today_and_tomorrow_events"
-        #today = timezone.now().replace(hour=0, minute=0, second=0)
-        #if not start_date or start_date.date() < today.date():
-        #    sqs = sqs.filter(recordings__media_file__isnull=False,
-        #                     recordings__state=Recording.STATUS.Published)
+         #FIXME: compare to code in  "today_and_tomorrow_events"
+        today = timezone.now().replace(hour=0, minute=0, second=0)
+        if not start_date or start_date.date() < today.date():
+            sqs = sqs.filter(recordings__media_file__isnull=False,
+                             recordings__state=Recording.STATUS.Published)
 
         if start_date:
             # Force hours to start of day
