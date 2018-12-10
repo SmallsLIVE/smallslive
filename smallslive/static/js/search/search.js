5efe1390891726b7453bd149db6ceeceead7250e
Ignacio Arzuaga <iarzuaga@sophilabs.com>
2018-12-10 14:32:35 -0300

    Fixed slider problem

--- smallslive/static/js/search/search.js
+++ smallslive/static/js/search/search.js
@@ -9,45 +9,43 @@ function fromYearClick(){
 function viewPortLength(viewPort) {
     var browserName = navigator.userAgent.toLowerCase(); 
     if (browserName.indexOf('safari') != -1) { 
-        if (browserName.indexOf('chrome') > -1) {
-            if( viewPort === "height" )
-                return window.innerHeight
-            else if( viewPort === "width" )
-                return window.innerWidth
-        } else {  
-            switch (window.orientation) {  
-                case 0:  
-                    if( viewPort === "height" )
-                        return window.innerWidth
-                    else if( viewPort === "width" )
-                        return window.innerHeight
-                    break;
-                    
-                case 180:  
-                    if( viewPort === "height" )
-                        return window.innerWidth
-                    else if( viewPort === "width" )
-                        return window.innerHeight
-                    break;
+        windowOrientation = (window.orientation, 90)
+        switch (windowOrientation) {  
+            case 0:  
+                if( viewPort === "height" )
+                    return window.innerWidth
+                else if( viewPort === "width" )
+                    return window.innerHeight
+                break;
                 
-                case -90:  
-                    if( viewPort === "height" )
-                        return window.innerHeight
-                    else if( viewPort === "width" )
-                        return window.innerWidth
-                    break;
-                
-                case 90:                  
-                    if( viewPort === "height" )
-                        return window.innerHeight
-                    else if( viewPort === "width" )
-                        return window.innerWidth
-                    break;
-            }
+            case 180:  
+                if( viewPort === "height" )
+                    return window.innerWidth
+                else if( viewPort === "width" )
+                    return window.innerHeight
+                break;
+            
+            case -90:  
+                if( viewPort === "height" )
+                    return window.innerHeight
+                else if( viewPort === "width" )
+                    return window.innerWidth
+                break;
+            
+            case 90:                  
+                if( viewPort === "height" )
+                    return window.innerHeight
+                else if( viewPort === "width" )
+                    return window.innerWidth
+                break; 
         }
+    }else{
+        if( viewPort === "height" )
+            return window.innerHeight
+        else if( viewPort === "width" )
+            return window.innerWidth
     }
 }
-
 function showQuantityDisplay(element, addition, slider){
     paginatorValue = $(element).data('paginator-number')
     maxValue = $(element).data('max-number')
