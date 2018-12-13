747b6873dce8796a3bd5c48089fd4cb2a97a88d3
Ignacio Arzuaga <iarzuaga@sophilabs.com>
2018-12-13 18:56:57 -0300

    started custom gateway for non logged buyers

--- smallslive/oscar_apps/checkout/forms.py
+++ smallslive/oscar_apps/checkout/forms.py
@@ -11,6 +11,10 @@ from oscar.apps.payment import forms as payment_forms
 from oscar.apps.order.models import BillingAddress
 from oscar.apps.address.models import UserAddress
 
+from oscar.apps.checkout.forms import GatewayForm as CoreGatewayForm
+from oscar.apps.customer.utils import normalise_email
+from email_validator import validate_email, EmailNotValidError
+
 
 STATE_CHOICES_WITH_EMPTY = (('', ''),) + STATE_CHOICES
 stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -156,3 +160,29 @@ class BillingAddressForm(payment_forms.BillingAddressForm):
 
     def validate_unique(self):
         pass
+
+
+
+class GatewayForm(CoreGatewayForm):
+    username = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=150, required=True,
+                                       help_text="First name for the guest list")
+    last_name = forms.CharField(max_length=150, required=True,
+                                       help_text="Last name for the guest list")
+
+    def clean(self):
+        if not self.is_guest_checkout():
+            if 'first_name' in self.errors:
+                del self.errors['first_name']
+            if 'last_name' in self.errors:
+                del self.errors['last_name']
+        return super(GatewayForm, self).clean()
+
+    def clean_username(self):
+        email = normalise_email(self.cleaned_data['username'])
+        try:
+            v = validate_email(email)
+            email = v["email"]
+        except EmailNotValidError as e:
+            raise forms.ValidationError("The email address is invalid. Perhaps there was a typo? Please try again.")
+        return email
