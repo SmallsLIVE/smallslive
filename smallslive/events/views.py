21e6792e8b1eee2d95d01d16f0b7ac81e147f2f7
Ignacio Arzuaga <iarzuaga@sophilabs.com>
2018-12-12 21:31:37 -0300

    Small fix

--- smallslive/events/views.py
+++ smallslive/events/views.py
@@ -402,7 +402,7 @@ class EventEditView(NamedFormsetsMixin, UpdateWithInlinesView):
             context['sets'].helper = EventSetInlineFormsetHelper()
         default_sets = []
         for default_set in ShowDefaultTime.objects.all():
-            default_sets.append({"set-venue" : default_set.venue, "set-starts": default_set.sets_start, "set-redeable-starts":  default_set.sets_readable_start, "set-duration": default_set.sets_readable_start, "set-title": default_set.title})
+            default_sets.append({"set-venue" : str(default_set.venue.name), "set-starts": default_set.sets_start(), "set-redeable-starts":  default_set.sets_readable_start(), "set-duration": default_set.set_duration, "set-title": str(default_set.title)})
         context['show_times'] = default_sets
         context['ticket_forms'] = self.construct_ticket_forms()
         return context
