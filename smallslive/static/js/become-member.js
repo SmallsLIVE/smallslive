506db3f15979d873fb0c6dd72b9d2b92772925b0
mmartinez <mmartinez@sophilabs.com>
2018-12-26 11:47:05 -0300

    deleted print

--- smallslive/static/js/become-member.js
+++ smallslive/static/js/become-member.js
@@ -126,7 +126,6 @@ $(document).ready(function () {
       type: $(this).attr('method'),
       data: $(this).serialize(),
       success: function( data ) {
-        console.log(data);
         $.get(data.url, function (data) {
           $.get(data.url, function (data) {
             $.get(data.url, function (data) {
