4cc42733386139d4fa5563ebe6825144772047ea
mmartinez <mmartinez@sophilabs.com>
2019-01-09 10:39:54 -0300

    store redesign

--- smallslive/oscar_apps/promotions/views.py
+++ smallslive/oscar_apps/promotions/views.py
@@ -16,6 +16,6 @@ class HomeView(promotions_views.HomeView):
         context['featured_physical_products'] = Product.objects.filter(
             product_class__slug="merchandise", featured=True)[:4]
         context['preview_track_id_counter'] = itertools.count()
-        context['artist_with_media'] = Artist.objects.annotate(media_count=Count('product')).filter(media_count__gt=0)
-                            
+        context['artist_with_media'] = Artist.objects.exclude(artistproduct=None)
+
         return context
