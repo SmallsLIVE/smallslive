b27b9fe4834bf6d8e0ad2bd094079f0a24e7e4fe
Ignacio Arzuaga <iarzuaga@sophilabs.com>
2019-01-10 15:17:51 -0300

    paginate wip progress

--- smallslive/oscar_apps/promotions/views.py
+++ smallslive/oscar_apps/promotions/views.py
@@ -10,6 +10,8 @@ class HomeView(promotions_views.HomeView):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['newest_recordings'] = Product.objects.filter(
             product_class__slug="album").order_by('-id')[:10]
+        context['all_recordings'] = Product.objects.filter(
+            product_class__slug="album")[:8]
         context['featured_recordings'] = Product.objects.filter(
             product_class__slug="album", featured=True)[:4]
         context['featured_physical_products'] = Product.objects.filter(
