570c4fdaed82aa3d8c589b25ec7b19a130bad018
mmartinez <mmartinez@sophilabs.com>
2019-01-15 15:58:11 -0300

    credit card month issue & events overlay fixed

--- smallslive/static/js/become-member.js
+++ smallslive/static/js/become-member.js
@@ -594,9 +594,15 @@ $(document).ready(function () {
 
   var checks = {
     '#card-number':  19, //4444 4444 4444 4444
-    '#expiry-month':  2,
     '#expiry-year':  2,
     '#cvc':  3,
+    '#expiry-month':  2
+  };
+  var checksFlow = {
+    '#card-number':  19, //4444 4444 4444 4444
+    '#expiry-month':  2,
+    '#expiry-year':  2,
+    '#cvc':  3
   };
 
   $(document).on('keyup', '#payment-form input', function (e) {
@@ -608,9 +614,9 @@ $(document).ready(function () {
     }
 
     var id = '#' + $(this).attr('id');
-    var keys = Object.keys(checks);
+    var keys = Object.keys(checksFlow);
     var pos = keys.indexOf(id);
-    if (pos + 1 < keys.length && $(this).val().length  == checks[id]) {
+    if (pos + 1 < keys.length && $(this).val().length  == checksFlow[id]) {
       if (id == '#expiry-year') {
         $('#name-on-card').focus();
       } else  {
@@ -628,9 +634,15 @@ $(document).ready(function () {
     var check = true;
     $.each(checks, function(selector, value){
       if (!checkInput(selector, value)) {
-        check = false;
-        return;
-      }
+        if(selector == '#expiry-month'){
+          if(parseInt($input.val()) < 13 && $input.val().trim() != ''){
+            return;
+            }
+          }
+          console.log('No ' + selector )
+          check = false;
+          return;
+        }
     });
 
     if (!check) {
