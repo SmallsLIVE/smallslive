2a853ae1bc6f01df7ada87dfb4068554050386a0
Ignacio Arzuaga <iarzuaga@sophilabs.com>
2019-03-01 15:01:09 -0300

    Added confimation prompt

--- smallslive/oscar_apps/customer/views.py
+++ smallslive/oscar_apps/customer/views.py
@@ -1,11 +1,19 @@
 from oscar.apps.customer.views import AccountRegistrationView as CoreAccountRegistrationView
+from django.shortcuts import get_object_or_404, redirect
+from allauth.account.app_settings import EmailVerificationMethod
+from allauth.account.utils import complete_signup
 
 
 class AccountRegistrationView(CoreAccountRegistrationView):
     def form_valid(self, form):
-        print "///"
-        print form
-        print "///"
-        return super(AccountRegistrationView, self).form_valid(form)
+        user = form.save(self.request)
+        # if self.kwargs['plan_name'] == 'free':
+        #     verification_method = EmailVerificationMethod.MANDATORY
+        # else:
+        #     verification_method = EmailVerificationMethod.OPTIONAL
+        verification_method = EmailVerificationMethod.OPTIONAL
+        complete_signup(self.request, user, verification_method,
+                        form.cleaned_data['redirect_url'])
+        return redirect(form.cleaned_data['redirect_url'])
 
     pass
