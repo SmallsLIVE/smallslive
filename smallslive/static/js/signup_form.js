e05255b2830ee9d90e65b892e4d6a78a593a83da
Martin Prunell <martin.prunell@gmail.com>
2020-03-04 18:56:14 -0300

    Privacy checkbox for sign up

--- smallslive/static/js/signup_form.js
+++ smallslive/static/js/signup_form.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   $memberForm = $('#form-become-member');
   $memberForm.submit(getAjaxSubmitForForm(
     $memberForm, ["first_name", "last_name", "email", "email2", "password1", "password2",
-      "terms_of_service"]
+      "terms_of_service", "privacy"]
   ));
 
 });
