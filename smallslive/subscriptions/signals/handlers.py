c53dc163418e684ee400ed32591d614904c46d5a
Martin Prunell <martin.prunell@gmail.com>
2020-04-08 21:44:23 -0300

    Fix stripe webhooks

--- smallslive/subscriptions/signals/handlers.py
+++ smallslive/subscriptions/signals/handlers.py
@@ -15,10 +15,11 @@ def invoice_payment_succeeded(sender, **kwargs):
     if event:
         customer = event.customer
         charge = event.message['data']['object']
+        invoice = charge['invoice']
         charge_id = charge['id']
-        amount = charge['amount']
+        amount = charge['amount'] / 100
         donation = subscriptions.models.Donation.objects.filter(reference=charge_id).first()
-        if not donation:
+        if not donation and invoice:
             donation = {
                 'user': customer.subscriber,
                 'currency': 'USD',
