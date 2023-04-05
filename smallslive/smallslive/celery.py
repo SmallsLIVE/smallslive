20996492ccca1e1241eb8770547a5343f2a8cd77
Rajib Paul <paul.rajib03@gmail.com>
2023-04-05 15:10:17 +0600

    Celery and its import added again

--- smallslive/smallslive/celery.py
+++ smallslive/smallslive/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 import os
 
-#from celery import Celery
+from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smallslive.settings.production')
