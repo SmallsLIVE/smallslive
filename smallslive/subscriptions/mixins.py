a83b53b4a5cc04d1662b24e7e605a09e545ca19f
Rajib Paul <paul.rajib03@gmail.com>
2023-11-24 22:48:45 +0600

    HOTFIX : payment issue in prod

--- smallslive/subscriptions/mixins.py
+++ smallslive/subscriptions/mixins.py
@@ -25,10 +25,14 @@ class PaymentCredentialsMixin(object):
                 print('hi there ....3')
                 is_foundation = False
                 venue = self.event.venue
-                stripe_client_id = venue.get_stripe_publishable_key
-                stripe_client_secret = venue.get_stripe_secret_key
-                paypal_client_id = venue.get_paypal_client_id
-                paypal_client_secret = venue.get_paypal_client_secret
+                # stripe_client_id = venue.get_stripe_publishable_key
+                # stripe_client_secret = venue.get_stripe_secret_key
+                # paypal_client_id = venue.get_paypal_client_id
+                # paypal_client_secret = venue.get_paypal_client_secret
+                stripe_client_id = settings.STRIPE_PUBLISHABLE_KEY  # venue.get_stripe_publishable_key
+                stripe_client_secret = settings.STRIPE_SECRET_KEY  # venue.get_stripe_secret_key
+                paypal_client_id = settings.PAYPAL_CLIENT_ID  # @TODO change with venue.get_paypal_client_id later.
+                paypal_client_secret = settings.PAYPAL_CLIENT_SECRET  # @TODO change with vennu.get_paypal_client_secret later.
         else:
             print('hi there ....4')
             if self.order:
