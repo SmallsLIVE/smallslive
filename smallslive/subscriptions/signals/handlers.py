607617316a256990131d6039bfb5f34f93cffdde
Rajib Paul <paul.rajib03@gmail.com>
2024-02-01 16:10:50 +0600

    webhook debuging

--- smallslive/subscriptions/signals/handlers.py
+++ smallslive/subscriptions/signals/handlers.py
@@ -28,13 +28,13 @@ def invoice_payment_succeeded(event, **kwargs):
 
         invoice = event.data
 
-        customer = invoice.customer
-        metadata = invoice.metadata
+        customer = invoice['customer']
+        metadata = invoice['metadata']
         if metadata and 'isFoundation' in metadata and not metadata['isFoundation']:
             return
 
-        charge_id = invoice.charge
-        amount = invoice.amount_paid / 100
+        charge_id = invoice['charge']
+        amount = invoice['amount_paid'] / 100
         donation = subscriptions.models.Donation.objects.filter(reference=charge_id).first()
         if not donation:
             donation = {
@@ -53,7 +53,7 @@ def invoice_payment_succeeded(event, **kwargs):
 
             if not customer.default_payment_method:
                 print('User have no default payment method')
-                payment_intent_id = invoice.payment_intent
+                payment_intent_id = invoice['payment_intent']
                 if payment_intent_id:
                     payment_intent = stripe.PaymentIntent.retrieve(payment_intent_id)
                     payment_method = payment_intent.payment_method
