d86ea7dd26f62d95aa813aabf95b776998c45fec
hassanmdsifat <hassansifat97@gmail.com>
2026-02-06 17:16:13 +0600

    - add docke compose watch for hot reload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-
 set -e
 
-echo "Waiting for postgres..."
+echo "Waiting for Postgres..."
 
+# Wait until Postgres is ready
 until nc -z "$DB_HOST" "$DB_PORT"; do
   echo "Postgres not ready..."
   sleep 2
@@ -11,7 +11,14 @@ done
 
 echo "Postgres ready"
 
-python manage.py collectstatic --noinput --clear
-python manage.py migrate --noinput
+# Run migrations only if MIGRATE_DB=true
+if [ "$MIGRATE_DB" = "true" ]; then
+  echo "Running database migrations..."
+  python manage.py collectstatic --noinput --clear
+  python manage.py migrate --noinput
+else
+  echo "Skipping database migrations"
+fi
 
+# Finally start CMD
 exec "$@"
