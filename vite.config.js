4072fd2b7ae543f690791da4ad3d3fb09821194e
hassanmdsifat <hassansifat97@gmail.com>
2026-02-19 22:24:41 +0600

    - base name setup

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/jazzculture/",
   plugins: [
     vue(),
     vueDevTools(),
