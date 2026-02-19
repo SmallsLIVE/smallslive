296fb9d48258b3fca86955316fad144bc1b92e97
hassanmdsifat <hassansifat97@gmail.com>
2026-02-19 22:56:36 +0600

    - remove base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/jazzculture/",
   plugins: [
     vue(),
     vueDevTools(),
